6b5d85ddbcbeeb95a1cc0d9d359195a00186f642
matthew-langley <matthew.langley@mail.utoronto.ca>
2017-07-14 13:22:44 -0400

    Indent error fixed

--- sccanalysis.py
+++ sccanalysis.py
@@ -150,16 +150,16 @@ if __name__ == '__main__':
             scc.graph['name'] = 'SCC' + str(len(sccList) + 1)
             sccList.append(scc)
         else:
-        # NB: Not all SCCs of size = 1 are steady states! Transition states
-        # count as SCCs too in the purest graph-theory sense.
-        # http://www.geeksforgeeks.org/strongly-connected-components/
-        # So need to check if SCC consists of only one node with only one
-        # (self-edge). i.e. set of 
-        if scc.edges() != []:
-            sourceNode = scc.edges()[0][0]
-            if set([sourceNode]) == set(G.successors(sourceNode)):
-                scc.graph['name'] = 'SS' + str(len(ssList) + 1)
-                ssList.append(scc)
+            # NB: Not all SCCs of size = 1 are steady states! Transition states
+            # count as SCCs too in the purest graph-theory sense.
+            # http://www.geeksforgeeks.org/strongly-connected-components/
+            # So need to check if SCC consists of only one node with only one
+            # (self-edge). i.e. set of 
+            if scc.edges() != []:
+                sourceNode = scc.edges()[0][0]
+                if set([sourceNode]) == set(G.successors(sourceNode)):
+                    scc.graph['name'] = 'SS' + str(len(ssList) + 1)
+                    ssList.append(scc)
     print 'Found %i strongly connected component(s) and %i steady states' %(len(sccList), len(ssList))
     
     # Calculate the expression profiles of each SCC and SS
